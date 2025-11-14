fd72ae97492d4de1fac3fbfe97abbc166ba70cc5
digitalfenixpro <admin@digitalfenixpro.com>
2025-11-14 14:51:30 -0500

    Updated translations.ts

--- src/utils/translations.ts
+++ src/utils/translations.ts
@@ -4,7 +4,7 @@ export type Language = 'es' | 'en';
 interface Translations {
   // Common
   save: string;
-  cancel: string;
+  cancela: string;
   edit: string;
   delete: string;
   create: string;
